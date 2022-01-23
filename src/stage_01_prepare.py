5e90324752a5a4936bcde187ac7f8b6218941a46
shamil-k <shamilvs789@gmail.com>
2022-01-23 23:43:35 +0530

    created trai ad test tsv

--- src/stage_01_prepare.py
+++ src/stage_01_prepare.py
@@ -6,6 +6,7 @@ from tqdm import tqdm
 import logging
 from src.utils.common import read_yaml, create_directories
 import random
+from src.utils.data_managmet import process_posts
 
 STAGE = 'One'
 
@@ -44,8 +45,8 @@ def main(config_path, params_path):
     with open(input_data, encoding = encode) as fd_in:
         with open(train_data_path, "w", encoding = encode) as fd_out_train:
             with open(test_data_path, "w", encoding = encode) as fd_out_test:
-                pass
-                # process_posts(fd_in, fd_out_train, fd_out_test, "<python>", split)
+              
+                process_posts(fd_in, fd_out_train, fd_out_test, "<python>", split)
 
 
 
