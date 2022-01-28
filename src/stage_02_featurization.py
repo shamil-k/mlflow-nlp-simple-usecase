740a86a6e962f2b4a879388f631ba428c8171dd6
shamil-k <shamilvs789@gmail.com>
2022-01-28 22:59:44 +0530

    st02 trainn data tfidf matrix save

--- src/stage_02_featurization.py
+++ src/stage_02_featurization.py
@@ -7,7 +7,7 @@ from src.utils.common import read_yaml, create_directories, get_df
 import random
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-
+from src.utils.featurize import save_matrix
 
 STAGE = "Two" 
 
@@ -41,8 +41,18 @@ def main(config_path, params_path):
     df_train = get_df(train_data_path)
 
     train_words = np.array(df_train.text.str.lower().values.astype("U"))
+    # print(train_words[:20])
 
-    print(train_words[:20])
+    bag_of_words = CountVectorizer(
+        stop_words="english", max_features = max_features, ngram_range = (1, ngrams))
+    
+    bag_of_words.fit_transform(train_words)
+    train_words_binary_matrix = bag_of_words.transform(train_words)
+
+    tfidf = TfidfVectorizer(smooth_idf=False)
+    tfidf.fit(train_words_binary_matrix)
+    train_words_tfidf_matrix = tfidf.transform(train_words_binary_matrix)
+    save_matrix(df_train, train_words_tfidf_matrix, featurized_train_data_path)
 
 
 if __name__ == '__main__':
