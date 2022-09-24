89423d376f4930db5a04f71508acb3258eb4b585
shamil-k <bareabhilash2000@gmail.com>
2022-09-24 15:45:02 +0530

    converted dvc to mlflow

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "dvc-nlp-simple-usecase"
+REPO_NAME = "mlflow-nlp-simple-usecase"
 AUTHOR_USER_NAME = "shamil-k"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = []
@@ -17,7 +17,7 @@ setup(
     description="Asked question regarding python or not by verifying the Tag from  Stack overflow",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/shamil-k/dvc-nlp-simple-usecase",
+    url="https://github.com/shamil-k/mlflow-nlp-simple-usecase",
     author_email="shamilvs789@gmail.com",
     packages=[SRC_REPO],
     license="MIT",
